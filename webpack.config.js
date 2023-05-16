8f2391fc7ca0ad4cdd922a4c8dd060ff5bdfebc7
unknown <awahremi@gmail.com>
2023-05-16 16:20:21 +0100

    Configuration files for Soul Mate done

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	  entry: './client/src/index.js',
+	  
+	  output: {
+		  path: path.resolve(__dirname, '/client.dist',),
+		  filename: 'bundle.js' ,
+	  },
+		name: 'browser',
+		mode: 'development',
+
+	  module: {
+		rules: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			include: [
+				path.resolve(__dirname, './client/dist')
+			],
+
+			options: {
+				presets: ['react'] 
+			},
+		}]
+	  }
+	  
+  };
+
